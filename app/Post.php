55502c831d2ab28befd94d38ec35b5681758c993
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-19 15:07:54 +0200

    creato il seeder per i post

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
-    //
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $fillable = ['title', 'content', 'slug'];
+
+    static public function generateSlug($originalStr) {
+        $baseSlug = Str::of($originalStr)->slug('-')->__toString();
+        $slug = $baseSlug;
+        $_i = 1;
+        while(self::where('slug', $slug)->first()) {
+            $slug = "$baseSlug-$_i";
+            $_i++;
+        }
+        return $slug;
+    }
 }
